acfc409521562f4f14cb598bdf2ccee017f3b130
Gabriel Guerrero <gabriel.guerrero@venturus.org.br>
2019-03-19 16:03:55 -0300

    Init Project

---
+++ Pensamentos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by Gabriel Carvalho Guerrero on 19/03/19.
+//  Copyright © 2019 Gabriel Carvalho Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
